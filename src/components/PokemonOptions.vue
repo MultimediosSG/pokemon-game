3ec2f32e3f6c9e9ec79b403e7b967a4badf23f7c
Alonso Chavarria <alons182@hotmail.com>
2023-06-15 09:27:16 -0600

    first commit

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,55 @@
+<script setup>
+defineProps({
+    pokemons: {
+        type: Array,
+        required: true
+    }
+});
+
+</script>
+<template>
+    <div class="options-container">
+        <ul>
+            <li>
+               opcion 1
+            </li>
+            <li>
+               opcion 2
+            </li>
+            <li>
+               opcion 3
+            </li>
+            <li>
+               opcion 4
+            </li>
+        </ul>
+
+    </div>
+</template>
+  
+
+  
+<style scoped>
+ul {
+    list-style-type: none;
+}
+
+li {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    margin-bottom: 10px;
+    width: 250px;
+}
+
+li:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+    display: flex;
+    justify-content: center;
+}
+</style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import usePokemonOptions from "@/composables/usePokemonOptions";
+
+import { onMounted, ref } from "vue";
+const { getPokemonOptions } = usePokemonOptions();
+
+const pokemonArr = ref([]);
+const pokemon = ref(null);
+const showPokemon = ref(false);
+const showAnswer = ref(false);
+const message = ref("");
+
+
+async function mixPokemonArray() {
+  
+}
+function checkAnswer(selectedId) {
+ 
+}
+function newGame() {
+ 
+}
+
+onMounted(() => {
+  mixPokemonArray()
+})
+
+</script>
+
+<template>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+
+  <div v-else>
+    <h1>¿Quién es este pokémon?</h1>
+
+    <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+
+    <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo Juego</button>
+    </template>
+  </div>
+</template>
+
